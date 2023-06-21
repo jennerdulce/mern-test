67231422b1d8a77c03f75f4e1f61af05c1b1b3b2
jennerdulce <jennerdulce@gmail.com>
2023-06-20 19:35:38 -0700

    added a new script that uses concurrently to run backend and frontend for deployment purposes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const colors = require('colors')
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const app = express()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
@@ -20,5 +20,13 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/goals', goalRoutes)
 app.use('/api/users', userRoutes)
 app.use(errorHandler)
+
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    // app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html')));
+  }
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
