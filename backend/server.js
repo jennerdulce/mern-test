a31e2903f5dd5847a11ff102ea14f3f5347315d7
jennerdulce <jennerdulce@gmail.com>
2023-06-22 16:58:00 -0700

    imported path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const colors = require('colors')
 const express = require('express')
 const dotenv = require('dotenv').config()
+const path = require('path')
 const port = process.env.PORT || 3001
 const app = express()
 const { errorHandler } = require('./middleware/errorMiddleware')
@@ -22,11 +23,11 @@ app.use('/api/users', userRoutes)
 app.use(errorHandler)
 
 if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    // app.use(express.static('client/build'));
-    app.use(express.static('frontend/build'));
+  //*Set static folder up in production
+  // app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html')));
-  }
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
