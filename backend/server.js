a755e2411ca7ff1080058476560734e152accfd9
jennerdulce <jennerdulce@gmail.com>
2023-06-22 17:08:44 -0700

    added build and deployment code

--- backend/server.js
+++ backend/server.js
@@ -20,14 +20,21 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', goalRoutes)
 app.use('/api/users', userRoutes)
-app.use(errorHandler)
 
 if (process.env.NODE_ENV === 'production') {
-  //*Set static folder up in production
-  // app.use(express.static('client/build'));
-  app.use(express.static('frontend/build'));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
 }
+
+
+app.use(errorHandler)
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
